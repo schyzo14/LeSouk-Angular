3d892c936681e377d41bff65254d09e08a5cf20c
Aurore <aurore.queille@free.fr>
2017-05-31 10:59:54 +0200

    > redirection page de garde vers connexion

--- app/scripts/controllers/pageDeGarde.js
+++ app/scripts/controllers/pageDeGarde.js
@@ -8,18 +8,27 @@
  * Controller of the leSoukApp
  */
 angular.module('leSoukApp')
-  .controller('PageDeGardeCtrl', ['$scope', '$cookies', 'AnnoncesCreesFactory', 'AnnoncesCandidateesFactory', 'UtilisateurFactory',
-	function ($scope, $cookies, AnnoncesCreesFactory, AnnoncesCandidateesFactory, UtilisateurFactory) {
+  .controller('PageDeGardeCtrl', ['$scope', '$cookies', 'AnnoncesCreesFactory', 'AnnoncesCandidateesFactory', 'UtilisateurFactory', '$location', 
+	function ($scope, $cookies, AnnoncesCreesFactory, AnnoncesCandidateesFactory, UtilisateurFactory, $location) {
 
-		$scope.user = {};
-		$scope.user.id = $cookies.get('idU');
-		
-		// Utilisateur
-		UtilisateurFactory.get({'idU' : $scope.user.id}).$promise.then(function(dataUtil) {
-			$scope.user.nom = dataUtil.nom;
-			$scope.user.prenom = dataUtil.prenom;
-			$scope.annoncesCrees = dataUtil.annoncesCreees;
-			$scope.annoncesCandidatees = dataUtil.annoncesCandidatees;
-       });		
+		// Si connecté
+		if($cookies.get('idU')!== undefined){
+	
+			$scope.user = {};
+			$scope.user.id = $cookies.get('idU');
+			
+			// Utilisateur
+			UtilisateurFactory.get({'idU' : $scope.user.id}).$promise.then(function(dataUtil) {
+				$scope.user.nom = dataUtil.nom;
+				$scope.user.prenom = dataUtil.prenom;
+				$scope.annoncesCrees = dataUtil.annoncesCreees;
+				$scope.annoncesCandidatees = dataUtil.annoncesCandidatees;
+		   });	
+
+		} else {
+            //Non connecté
+            $location.path('/');
+        }
+	   
 	}
 ]);
