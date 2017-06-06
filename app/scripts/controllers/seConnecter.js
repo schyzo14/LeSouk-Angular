a97cf63cc8bc6d5d28bf467dc6a295cd2f455fdd
Aurore <aurore.queille@free.fr>
2017-06-06 18:55:06 +0200

    Spring security

--- app/scripts/controllers/seConnecter.js
+++ app/scripts/controllers/seConnecter.js
@@ -8,64 +8,50 @@
  * Controller of the clientApp
  */
 angular.module('leSoukApp')
-    .controller('SeConnecterCtrl', ['$location', '$scope', '$cookies', 'ProfilFactory', 'Connexion', '$window', 'AuthentificationFactory',
-      function ($location, $scope, $cookies, ProfilFactory, Connexion, $window, AuthentificationFactory) {
+    .controller('SeConnecterCtrl', ['$http', '$location', '$scope', '$cookies', 'ProfilFactory', 'Connexion', '$window', 'AuthentificationFactory',
+      function ($http, $location, $scope, $cookies, ProfilFactory, Connexion, $window, AuthentificationFactory) {
     $scope.data = {};
 
     $scope.seConnecterF = function () {
-			var nomUtil = $scope.data.nomUtil;
-            var mdpUtil = $scope.data.password;
-			
-			var authenticated = false;
+		var nomUtil = $scope.data.nomUtil;
+		var mdpUtil = $scope.data.password;
+		
+		var authenticated = false;
         
         if(nomUtil!==undefined){
-			
-			var info = btoa(nomUtil + ":" + mdpUtil);
-			
-			var auth = new AuthentificationFactory({
-						data : info
-					 });
-			auth.$save(function(data) {
-				
-				if (data.name) {
-					authenticated = true;
-				} else {
-					authenticated = false;
-				}
-				
-				alert(authenticated);
-				alert(data);
+	
+			this.user = undefined;
+			var requestParams = {};
+
+			var headers = {
+				authorization: 'Basic ' + btoa(nomUtil + ":" + mdpUtil)
+			};
+			var requestParams = {headers: headers};
+
+			$http.get('http://localhost:8080/api/seConnecter', requestParams).then(function (response) {
+				this.user = response.data;
+				return this.user;
+			}.bind(this));
 			
             //GET
-				ProfilFactory.get({'pseudoU' : nomUtil}).$promise.then(function(data) {
-					var mdpU = data.mdp;
-					var idU = data.id;
-					
-					if(mdpU===mdpUtil){
-						$cookies.put('idU', idU);
-						Connexion.setUser(idU);
-						
-						$location.path('/compte');
-					}else{
-						$window.alert("GET foireux");
-					}
-					
-				}).catch(function() {
-					$window.alert("L'utilisateur "+nomUtil+" n'existe pas.");
-					/** redirection page principale **/
-					$location.path('/');
-				});
+			ProfilFactory.get({'pseudoU' : nomUtil}).$promise.then(function(data) {
 
+					$cookies.put('idU', data.id);
+					Connexion.setUser(data.id);
+					
+					$location.path('/compte');
 				
 			}).catch(function() {
 				$window.alert("L'utilisateur "+nomUtil+" n'existe pas.");
-                /** redirection page principale **/
-                $location.path('/');
-            });
+				/** redirection page principale **/
+				$location.path('/');
+			});
+
+				
         }else{
             $window.alert("Aucun utilisateur renseigné!");
         }
 
-						
-    };
+	  };	
+    
 }]);
